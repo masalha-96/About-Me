78bafff3c9464e2526f21e7f665afbf2b158c244
Masalha-96 <Smasalha96@gmail.com>
2021-04-26 21:03:02 +0300

    lastupdate

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@
 alert('Hello');
 let  userName = prompt('What is your name?');
 
+let ctr =0; // marks counter
 
-//Q1
+//QQ
 let userAnswer0 =  prompt('Welcome To My Page, ' + userName + ' , have you visited my site before?' );
 switch(userAnswer0.toUpperCase())
 {
@@ -20,105 +21,132 @@ case 'N':
 
     default:
         //console.log('Inviled Answer');
-        alert('Invalid input, answer with Y/N ')
-        let userAnswer0 =  prompt('Welcome To My Page, ' + userName + ' , have you visited my site before?' );
+        alert('You have lost this question, please make sure to answer yes or no to the next questions' );
         break;
 
 }
 
 
-//Q2
-let userAnswer1 = prompt('Do you know me?');
+alert('After This Message, We\'re going to play a quick game, called Guess-aboutMe, Which is made up of 5 Questions that you will answer with (yes or no)');
+
+
+//QQ1
+let userAnswer1 = prompt('Q1:: You think I was born in 1996?');
 switch(userAnswer1.toUpperCase())
 {
 case 'YES':
 case 'Y':
-    alert('Ya Hala Beek!')
+    ctr++;
+    alert('Correct Answer, I was born in 16 August 1996')
     break;
 
 case 'NO':
 case 'N':
-    alert('You can check out the page, to get to know me a little')
+    alert('Wrong Answer')
     break;
 
     default:
-         //console.log('Inviled Answer');
-        alert('Invalid input, answer with Y/N ')
-        let userAnswer1 = prompt('Do you know me?');
+        //console.log('Inviled Answer');
+        alert('You have lost this question, please make sure to answer yes or no to the next questions' );
         break;
 
 }
 
-//Q3
-let userAnswer3 = prompt('Are you a social person??');
+//QQ2
+let userAnswer3 = prompt('Q2:: Do you think I studied at the University of Jordan???');
 
 switch(userAnswer3.toUpperCase())
 {
 case 'YES':
 case 'Y':
-    alert ('It would be a great pleasure to get to know you ^^ ');
+    alert ('Wrong Answer, I studied at Al-Balqa Applied University');
     break;
 
 case 'NO':
 case 'N':
-    alert (' قول وغير ') ;
+    ctr++;
+    alert ('Correct Answer') ;
     break;
 
     default:
          //console.log('Inviled Answer');
-        alert('Invalid input, answer with Y/N ')
-        let userAnswer3 = prompt('Are you a social person??');
-        break;
+         alert('You have lost this question, please make sure to answer yes or no to the next questions' );         
+         break;
 
 }
 
-//Q4
-let userAnswer2 = prompt('Do you like mansaf?');
+//QQ3
+let userAnswer2 = prompt(' Q3:: Do you think I studied law??');
 switch(userAnswer2.toUpperCase())
 {
 case 'YES':
 case 'Y':
-    alert('me too :P')
+    alert('Wrong Answer, I studied Computer Engineering');
     break;
 
 case 'NO':
 case 'N':
-    alert('Oh, sad news :(')
+    ctr++;
+    alert('Correct Answer, I\'m a Computer Engineer')
     break;
 
     default:
          //console.log('Inviled Answer');
-        alert('Invalid input, answer with Y/N ')
-        let userAnswer2 = prompt('Do you like mansaf?');
-        break;
+         alert('You have lost this question, please make sure to answer yes or no to the next questions' );         
+         break;
 }
 
 
 
 
-//Q5
-let userAnswer4 = prompt('Do you love MATH?');
+//QQ4
+let userAnswer4 = prompt('Do you think that I\'m from Amman?');
 
 switch(userAnswer4.toUpperCase())
 {
 case 'YES':
 case 'Y':
-    alert (' nice ');
+    alert ('Wrong Answer, I deceived you:P I\'m from Al-Balqa');
     break;
 
 case 'NO':
 case 'N':
-    alert (' me too  ') ;
+    ctr++;
+    alert ('Correct Answer! I\'m from Al-Balqaa - Deir Alla ') ;
     break;
 
     default:
          //console.log('Inviled Answer');
-        alert('Invalid input, answer with Y/N ')
-        let userAnswer4 = prompt('Do you know me?'); 
-        break;
+         alert('You have lost this question, please make sure to answer yes or no to the next questions' );         
+         break;
 
 }
 
 
 
+//QQ5
+let userAnswer5 = prompt('Do you think I\'ll get a full mark on this assignment?');
+
+switch(userAnswer5.toUpperCase())
+{
+case 'YES':
+case 'Y':
+    ctr++;
+    alert ('Correct Answer! كم انت كريم يا رب');
+    break;
+
+case 'NO':
+case 'N':
+    alert ('Please write notes for me in the comments .. to improve myself') ;
+    break;
+
+    default:
+         //console.log('Inviled Answer');
+         alert('You have lost this question, please make sure to answer yes or no to the next questions' );         
+         break;
+
+}
+
+
+alert('Thank You, ' + userName + ' Your score is ( ' + ctr +' / 5 ) ');
 
